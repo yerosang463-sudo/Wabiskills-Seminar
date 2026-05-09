39e983cc057aa411059c6fea5b74308a4accb004
yero <yerosang463@gmail.com>
2026-05-10 05:18:12 +0900

    make sure correct google client id for authetication

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -15,7 +15,7 @@ const config = {
   google: {
     clientId: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackUrl: process.env.GOOGLE_CALLBACK_URL || '/api/auth/google/callback',
+    callbackUrl: '/api/auth/google/callback',
   },
   cors: {
     origin:
